efdeb632d7e664db0f15a31fd901c6763b116939
Rzd <140939924+MDrzd@users.noreply.github.com>
2026-07-30 19:00:41 +0700

    Update versionName to 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2.2"
+        versionName = "1.2.5"
 
         vectorDrawables {
             useSupportLibrary = true
